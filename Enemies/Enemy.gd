be65703db7560a64c5f1b0063d35fc71a78aa762
Indieology <contactindieology@gmail.com>
2022-09-08 00:16:58 -0500

    Enemy run and attack

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -1,9 +1,11 @@
 extends KinematicBody2D
 
 export var health = 2
-export var speed = 1500
+export var speed = 80
 export var distance_to_attack = 60
 
+onready var player_animation = $AnimatedSprite
+onready var attack_timer = $AttackTimer
 onready var player = get_parent().get_node("Player")
 
 var velocity = Vector2.ZERO
@@ -18,13 +20,14 @@ func _process(delta):
 func _physics_process(delta):
 	velocity = Vector2.ZERO
 	
-	velocity.y += 9000 #gravity
+	velocity.y += 150 #gravity
 	if is_on_floor():
 		if player.global_position.x - global_position.x > distance_to_attack or player.global_position.x - 	global_position.x < distance_to_attack * -1:
 			velocity.x = position.direction_to(player.position).x * speed
+			player_animation.play("Run")
 		else:
 			attack()
-	move_and_slide(velocity * delta, Vector2.UP)
+	move_and_slide(velocity, Vector2.UP)
 	
 	
 func _on_HurtDetector_area_entered(area):
@@ -32,4 +35,13 @@ func _on_HurtDetector_area_entered(area):
 		queue_free()
 
 func attack():
-	pass
+	if attack_timer.is_stopped():
+		player_animation.play("Attack")
+		attack_timer.start()
+
+
+func _on_AnimatedSprite_animation_finished():
+	if velocity.x == 0:
+		player_animation.play("Idle")
+	else:
+		player_animation.play("Run")
